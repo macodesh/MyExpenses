4f291e7225ffbbfbb8458cb19982356cb6529aa1
Marlon Couto <devpatola@proton.me>
2023-08-17 16:21:30 -0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Entre com o número de despesas: ");
+        int numberOfExpenses = GetNumberOfExpenses();
+
+        int[,] expensesCost = new int[numberOfExpenses, 2];
+        for (int i = 0; i < numberOfExpenses; i++)
+        {
+            for (int j = 0; j < 2; j++)
+            {
+                expensesCost[i, j] = GetExpenseCostFromUser();
+            }
+        }
+
+        int totalExpenses = 0;
+        for (int i = 0; i < numberOfExpenses; i++)
+        {
+            for (int j = 0;j < 2; j++)
+            {
+                totalExpenses += expensesCost[i, j];
+            }
+        }
+
+        Console.WriteLine("O total das despesas é: " + totalExpenses);
+    }
+
+    static int GetNumberOfExpenses()
+    { return 5; }
+
+    static int GetExpenseCostFromUser()
+    { return 3; }
+}
