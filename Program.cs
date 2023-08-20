e1e1fc293a3740632d8083c0c5a2bd1f4cf1fefc
Marlon Couto <devpatola@proton.me>
2023-08-20 19:35:10 -0300

    Atualiza Program.cs

--- Program.cs
+++ Program.cs
@@ -1,34 +1,24 @@
-﻿class Program
+﻿Console.WriteLine("Entre com o número de despesas: ");
+int numberOfExpenses = GetNumberOfExpenses();
+
+int[,] expensesCost = new int[numberOfExpenses, 2];
+for (int i = 0; i < numberOfExpenses; i++)
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Entre com o número de despesas: ");
-        int numberOfExpenses = GetNumberOfExpenses();
-
-        int[,] expensesCost = new int[numberOfExpenses, 2];
-        for (int i = 0; i < numberOfExpenses; i++)
-        {
-            for (int j = 0; j < 2; j++)
-            {
-                expensesCost[i, j] = GetExpenseCostFromUser();
-            }
-        }
-
-        int totalExpenses = 0;
-        for (int i = 0; i < numberOfExpenses; i++)
-        {
-            for (int j = 0;j < 2; j++)
-            {
-                totalExpenses += expensesCost[i, j];
-            }
-        }
-
-        Console.WriteLine("O total das despesas é: " + totalExpenses);
-    }
-
-    static int GetNumberOfExpenses()
-    { return 5; }
-
-    static int GetExpenseCostFromUser()
-    { return 3; }
+    for (int j = 0; j < 2; j++)
+        expensesCost[i, j] = GetExpenseCostFromUser();
 }
+
+int totalExpenses = 0;
+for (int i = 0; i < numberOfExpenses; i++)
+{
+    for (int j = 0; j < 2; j++)
+        totalExpenses += expensesCost[i, j];
+}
+
+Console.WriteLine("O total das despesas é: " + totalExpenses);
+
+int GetNumberOfExpenses()
+{ return 5; }
+
+int GetExpenseCostFromUser()
+{ return 3; }
