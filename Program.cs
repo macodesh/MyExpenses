92b8b276d6db2938c55a79bb616c4d2ece7bc042
Marlon Couto <125909621+macodesh@users.noreply.github.com>
2023-08-26 10:33:40 -0300

    Atualizar o Program.cs

--- Program.cs
+++ Program.cs
@@ -1,18 +1,22 @@
-﻿Console.WriteLine("Entre com o número de despesas: ");
+Console.WriteLine("Entre com o número de despesas: ");
 int numberOfExpenses = GetNumberOfExpenses();
 
 int[,] expensesCost = new int[numberOfExpenses, 2];
 for (int i = 0; i < numberOfExpenses; i++)
 {
     for (int j = 0; j < 2; j++)
+    {
         expensesCost[i, j] = GetExpenseCostFromUser();
+    }
 }
 
 int totalExpenses = 0;
 for (int i = 0; i < numberOfExpenses; i++)
 {
     for (int j = 0; j < 2; j++)
+    {
         totalExpenses += expensesCost[i, j];
+    }
 }
 
 Console.WriteLine("O total das despesas é: " + totalExpenses);
